29c9e0fcdd3157c7de54d351eba2f2219581adf4
marouanemseddi <marouanemseddi4@gmail.com>
2022-08-23 18:47:14 +0200

    adding service

--- src/main/java/com/clinysys/management/service/PatientService.java
+++ src/main/java/com/clinysys/management/service/PatientService.java
@@ -1,5 +1,42 @@
 package com.clinysys.management.service;
 
-public class PatientService {
 
-}
+	import java.util.ArrayList;  
+	import java.util.List;  
+	import org.springframework.beans.factory.annotation.Autowired;  
+	import org.springframework.stereotype.Service;
+
+import com.clinysys.management.model.Patient;
+import com.clinysys.management.repository.PatientRepository;
+
+
+	//defining the business logic  
+	@Service  
+	public class PatientService   
+	{  
+		@Autowired  
+		PatientRepository patientRepository;  
+		//getting all Patient record by using the method findaAll() of CrudRepository  
+		public List<Patient> getAllPatient()   
+		{  
+			List<Patient> patients = new ArrayList<Patient>();  
+			patientRepository.findAll().forEach(Patient1 -> patients.add(Patient1));  
+			return patients;  	
+		}  
+		//getting a specific record by using the method findById() of CrudRepository  
+		public Patient getPatientById(int id)   
+		{  
+			return patientRepository.findById(id).get();  
+		}  
+		//saving a specific record by using the method save() of CrudRepository  
+		public void saveOrUpdate(Patient Patient)   
+		{  
+			patientRepository.save(Patient);  
+		}  
+		//deleting a specific record by using the method deleteById() of CrudRepository  
+		public void delete(int id)   
+		{  
+			patientRepository.deleteById(id);  
+		}  
+
+	}  
